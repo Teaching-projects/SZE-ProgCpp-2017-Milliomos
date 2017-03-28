90b993b87de55aa6f4f59e652a4250e4721d2834
szabopown <nostradamusxadpz8@gmail.com>
2017-03-28 13:39:25 +0200

    Elkezdtük

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h> 
+#include <math.h>
+#include <time.h>
+#include <istream>
+#include <fstream>
+#include "magyarit.h"
+
+using namespace std;
+#define MAX 512
+
+int main() {
+	magyarit();
+	cout << "\nÜdvözöljük a Legyen Ön is Milliomos játékban!\n\n";
+	cout << "\n|___Menu ___|\n\nÚj játék (J)\nÚtmutató játékhoz(U)\nSzabályzat(S)\nToplista(T)\nKilépés(K)\n";
+	
+
+
+	
+	
+
+	return 0;
+}
