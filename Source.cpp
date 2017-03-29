2710184d857a6a55a039e35807a99124a37ba9a9
szabopown <nostradamusxadpz8@gmail.com>
2017-03-29 11:44:23 +0200

    Szabályzat hozzáadva

--- Source.cpp
+++ Source.cpp
@@ -14,10 +14,33 @@ using namespace std;
 #define MAX 512
 
 int main() {
-	magyarit();
-	cout << "\nÜdvözöljük a Legyen Ön is Milliomos játékban!\n\n";
-	cout << "\n|___Menu ___|\n\nÚj játék (J)\nÚtmutató játékhoz(U)\nSzabályzat(S)\nToplista(T)\nKilépés(K)\n";
+	int kilep = 0, ok;
 	
+	std::string s;
+	
+	magyarit();
+		while (kilep != 2) {
+			system("cls"); 
+			cout << "\nÜdvözöljük a Legyen Ön is Milliomos játékban!\n\n";
+			cout << "\n|___Menu ___|\n\nÚj játék (J)\nÚtmutató játékhoz(U)\nSzabályzat(S)\nToplista(T)\nKilépés(K)\n";
+			do {
+				ok = 1;
+				cout << "\n";
+				std::getline(std::cin, s);
+				char c = s[0];
+				s = toupper(c);
+				if ((s.compare("J") != 0) && (s.compare("S") != 0) && (s.compare("T") != 0) && (s.compare("K") != 0) && (s.compare("U") != 0)) { cout << "Helytelen Karakter!\n"; ok = 0; }
+			} while (!ok);
+			if (s.compare("S") == 0) {
+				system("cls");
+				cout << "A székbe kerülõ játékosnak 15 egyre nehezedõ kérdést tesznek fel.\nA kérdések feleletválasztósak: négy válaszlehetõséget is megadnak,\nmelyek közül a játékosnak kell kiválasztania a helyeset.\n(A négy válaszlehetõséget a latin ábécé elsõ négy betûjével jelölik.)\nAz elsõ kérdés helyes megválaszolásával a játékos 5 000 Ft-ot nyer.\nA többi kérdéssel egyre több pénzhez jut a játékos.\nNagyjából minden kérdés után megduplázódik a nyeremény.";
+				cout << "\n\nEnterrel tud visszalépni a fõmenübe!\n";
+				getchar();
+			}
+	}
+	
+	
+
 
 
 	
