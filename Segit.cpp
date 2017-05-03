37eb74f8a029798f11935ecf5976271f6647439a
HerenkovicsA <herenkovics95@gmail.com>
2017-05-03 19:43:12 +0200

    Segítség javítva
    Felezés utáni, de azonos szinten használt telefon/közönség csak a
    megmaradt válaszlehetőségekből választanak

--- Segit.cpp
+++ Segit.cpp
@@ -2,7 +2,7 @@
 
 
 
-Segitseg::Segitseg(int k, int t, int f) : kozonseg{ k }, telefon{t},felezes { f } {
+Segitseg::Segitseg(int k, int t, int f, int fszint, char jo, char roszz) : kozonseg{ k }, telefon{t},felezes { f } {
 	
 }
 
@@ -15,7 +15,16 @@ int Segitseg::getFelezes() {
 int Segitseg::getTelefont() {
 	return telefon;
 }
+int Segitseg::getFelezSzint() {
+	return fszint;
+}
+char Segitseg::getRossz() {
+	return rossz;
+}
 
+void Segitseg::setFelezSzint(int sz) {
+	fszint = sz;
+}
 void Segitseg::setKozonseg(int sz) {
 	this->kozonseg = sz;
 }
@@ -24,6 +33,9 @@ void Segitseg::setFelezes(int sz) {
 }
 void Segitseg::setTelefon(int sz) {
 	this->telefon = sz;
+}
+void Segitseg::setRossz(char r) {
+	rossz = r;
 }
 
 std::string Segitseg::BetuSzam(int x) {
@@ -45,7 +57,7 @@ std::string Segitseg::BetuSzam(int x) {
 }
 
 void Segitseg::Felez(node *conductor) {
-	
+	setFelezSzint(conductor->nehez);
 	if (getFelezes()==0)
 	{
 		cout << "Nincs több felezés segítsége" << std::endl;
@@ -71,36 +83,86 @@ void Segitseg::Felez(node *conductor) {
 		if (rossz.compare("A") == 0 && conductor->valasz.compare("B") == 0 || rossz.compare("B") == 0 && conductor->valasz.compare("A") == 0)
 		{
 			cout << "(A) " << setw(30) << left << conductor->a << "(B)" << conductor->b << std::endl; //jó
+			if (conductor->valasz.compare("A") == 0)
+			{
+				setRossz('B');
+			}
+			else
+			{
+				setRossz('A');
+			}
 		}
 		else if (rossz.compare("A") == 0 && conductor->valasz.compare("C") == 0 || rossz.compare("C") == 0 && conductor->valasz.compare("A") == 0)
 		{
-			cout << "(A) " << setw(30) << left << conductor->a << "\n(C) " << setw(30) << left << conductor->c << std::endl; //jó
+			cout << "(A) " << setw(30) << left << conductor->a << "\n(C) " << setw(30) << left << conductor->c << std::endl;
+			if (conductor->valasz.compare("A") == 0)
+			{
+				setRossz('C');
+			}
+			else
+			{
+				setRossz('A');
+			}
+
 		}
 		else if (rossz.compare("A") == 0 && conductor->valasz.compare("D") == 0 || rossz.compare("D") == 0 && conductor->valasz.compare("A") == 0)
 		{
 			cout << "(A) " << setw(30) << left << conductor->a << std::endl << setw(30) << left << "" << "(D)" << conductor->d << std::endl;
+			if (conductor->valasz.compare("A") == 0)
+			{
+				setRossz('D');
+			}
+			else
+			{
+				setRossz('A');
+			}
 		}
 		else if (rossz.compare("B") == 0 && conductor->valasz.compare("C") == 0 || rossz.compare("C") == 0 && conductor->valasz.compare("B") == 0)
 		{
-			cout << setw(30) << left << "" << "(B)" << conductor->b << "\n(C) " << setw(30) << left << conductor->c << std::endl; //jó
+			cout << setw(30) << left << "" << "(B)" << conductor->b << "\n(C) " << setw(30) << left << conductor->c << std::endl; 
+			if (conductor->valasz.compare("B") == 0)
+			{
+				setRossz('C');
+			}
+			else
+			{
+				setRossz('B');
+			}
 		}
 		else if (rossz.compare("B") == 0 && conductor->valasz.compare("D") == 0 || rossz.compare("D") == 0 && conductor->valasz.compare("B") == 0)
 		{
-			cout << setw(30) << left << "" << "(B)" << conductor->b << std::endl << setw(30) << left << "" << "(D)" << conductor->d << std::endl; //jó
+			cout << setw(30) << left << "" << "(B)" << conductor->b << std::endl << setw(30) << left << "" << "(D)" << conductor->d << std::endl; 
+			if (conductor->valasz.compare("B") == 0)
+			{
+				setRossz('D');
+			}
+			else
+			{
+				setRossz('B');
+			}
 		}
 		else if (rossz.compare("C") == 0 && conductor->valasz.compare("D") == 0 || rossz.compare("D") == 0 && conductor->valasz.compare("C") == 0)
 		{
-			cout << std::endl << "(C)" << setw(30) << left << conductor->c << "(D)" << conductor->d << std::endl; //jó
+			cout << std::endl << "(C)" << setw(30) << left << conductor->c << "(D)" << conductor->d << std::endl; 
+			if (conductor->valasz.compare("C") == 0)
+			{
+				setRossz('D');
+			}
+			else
+			{
+				setRossz('C');
+			}
 		}
 	}
 }
 
-void Segitseg::Kozonseg(int szint, node *conductor) {
+void Segitseg::Kozonseg(node *conductor) {
 	if (getKozonseg() == 0)
 	{
 		cout << "A közönség segítséget már elhasználta" << std::endl;
 	}
 	else {
+		int szint = conductor->nehez;
 		double h_tipp;
 		double rossz;
 		int ok = 0;
@@ -108,15 +170,22 @@ void Segitseg::Kozonseg(int szint, node *conductor) {
 		setKozonseg(0);
 		if (h_tipp > 60)
 		{
-			while (!ok)
+			if (getFelezSzint() == conductor->nehez)
 			{
-				rossz = rand() % 4;
-				if (BetuSzam(rossz).compare(conductor->valasz) != 0)
+				std::cout << "A közönség szerint a helyes válasz: " << getRossz() << std::endl;
+			}
+			else
+			{
+				while (!ok)
 				{
-					cout << "A közönség szerint a helyes válasz: " << BetuSzam(rossz) << std::endl;
-					ok = 1;
-				}
+					rossz = rand() % 4;
+					if (BetuSzam(rossz).compare(conductor->valasz) != 0)
+					{
+						cout << "A közönség szerint a helyes válasz: " << BetuSzam(rossz) << std::endl;
+						ok = 1;
+					}
 
+				}
 			}
 		}
 		else
@@ -151,15 +220,22 @@ void Segitseg::Telefon(int szint, node *conductor) {
 		cout << "\n";
 		if (t_tipp > 70)
 		{
-			while (!ok)
+			if (getFelezSzint() == conductor->nehez)
 			{
-				rossz = rand() % 4;
-				if (BetuSzam(rossz).compare(conductor->valasz) != 0)
+				std::cout << "Szerintem a helyes válasz: " << getRossz() << std::endl;
+			}
+			else
+			{
+				while (!ok)
 				{
-					cout << "Szerintem a helyes válasz: " << BetuSzam(rossz) << std::endl;
-					ok = 1;
-				}
+					rossz = rand() % 4;
+					if (BetuSzam(rossz).compare(conductor->valasz) != 0)
+					{
+						cout << "Szerintem a helyes válasz: " << BetuSzam(rossz) << std::endl;
+						ok = 1;
+					}
 
+				}
 			}
 		}
 		else
--- segit.h
+++ segit.h
@@ -12,19 +12,24 @@ class Segitseg
 {
 
 private:
-	int kozonseg, telefon, felezes;
+	int kozonseg, telefon, felezes, fszint;
+	char jo, rossz;
 
 public:
-	Segitseg(int k=1, int t=1, int f=1);
+	Segitseg(int k = 1, int t = 1, int f = 1, int fszint = -1, char jo = 'Z', char rossz ='Z');
 	int getKozonseg();
 	int getTelefont();
 	int getFelezes();
+	int getFelezSzint();
+	char getRossz();
+	void setFelezSzint(int sz);
 	void setKozonseg(int sz);
 	void setTelefon(int sz);
 	void setFelezes(int sz);
 	void Felez(node *conductor);
-	void Kozonseg(int szint, node *conductor);
+	void Kozonseg(node *conductor);
 	void Telefon(int szint, node *conductor);
+	void setRossz(char r);
 	std::string BetuSzam(int x);
 
 };
